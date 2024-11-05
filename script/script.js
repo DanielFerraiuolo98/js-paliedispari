b00a4e242f6d950f79081fb409793857215a6b4d
DanielFerraiuolo98 <daniel.ferraiuolo.98@gmail.com>
2024-11-05 15:50:01 +0100

    esercizio pari e dispari completato

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma*/
 
-const text = prompt("scrivi una parola");
+/*const text = prompt("scrivi una parola");
 const reversedText = parolaPalindroma(text);
 
 if(text.toLowerCase = reversedText.toLowerCase()){
@@ -21,7 +21,43 @@ function parolaPalindroma(parola) {
         palindroma = palindroma + parola[i];
     }
     return palindroma;
-} 
+} */
+
+
+    /*Pari e Dispari
+    L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+    Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+    Sommiamo i due numeri
+    Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+    Dichiariamo chi ha vinto.*/
+
+    const userChoice = prompt("scegli pari o dispari");
+    const numUser = parseInt(prompt("inserisci un numero da 1 a 5")); 
+    console.log(numUser);
+    const numPc = generaNumeroRandom(1,5);
+    const somma = numPc + numUser;
+    const pariDispari = pariODispari(somma);
+    console.log(pariDispari);
+
+    if(pariDispari === userChoice){
+        console.log("hai vinto");
+    }
+    else{
+        console.log("hai perso");
+    }
+
+    function generaNumeroRandom(min, max){
+        const numeroRandom = Math.floor(Math.random() * (max - min + 1)) + min;
+        console.log(numeroRandom);
+    }
    
+    function pariODispari(num){
+        if(num % 2 === 0){
+            return "pari"
+        }
+        else{
+            return "dispari"
+        }
+    }
 
 
